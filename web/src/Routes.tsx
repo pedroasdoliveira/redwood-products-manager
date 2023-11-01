38906f5503bbfc24b8db9f8bae5f7cdb6286c246
Pedro Oliveira <Pedroaolive@gmail.com>
2023-11-01 17:51:40 -0300

    feat: finished Products page

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -6,21 +6,19 @@
 //
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
+import { Route, Router } from '@redwoodjs/router';
 
-import { Set, Router, Route } from '@redwoodjs/router'
-
-import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
 
 const Routes = () => {
   return (
     <Router>
       <Route path="/" redirect="/products" />
-      <Set wrap={ScaffoldLayout} title="Products" titleTo="products" buttonLabel="New Product" buttonTo="newProduct">
-        <Route path="/products/new" page={ProductNewProductPage} name="newProduct" />
-        <Route path="/products/{id:Int}/edit" page={ProductEditProductPage} name="editProduct" />
-        <Route path="/products/{id:Int}" page={ProductProductPage} name="product" />
-        <Route path="/products" page={ProductProductsPage} name="products" />
-      </Set>
+
+      <Route path="/products/new" page={ProductNewProductPage} name="newProduct" />
+      <Route path="/products/{id:Int}/edit" page={ProductEditProductPage} name="editProduct" />
+      <Route path="/products/{id:Int}" page={ProductProductPage} name="product" />
+      <Route path="/products" page={ProductProductsPage} name="products" />
+
       <Route notfound page={NotFoundPage} />
     </Router>
   )
--- web/src/components/Product/Products/Products.tsx
+++ web/src/components/Product/Products/Products.tsx
@@ -1,15 +1,19 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Container } from '@chakra-ui/react'
+import './style.css';
+
+import { SearchIcon, SmallAddIcon } from '@chakra-ui/icons';
+import { Box, Center, Container, Flex, Grid, GridItem, Heading, IconButton, Input, Stack, Text } from '@chakra-ui/react';
 import type {
   DeleteProductMutationVariables,
   FindProducts,
 } from 'types/graphql'
 
-import { useMutation } from '@redwoodjs/web'
-import { toast } from '@redwoodjs/web/toast'
+import { Link, routes } from '@redwoodjs/router';
+import { useMutation } from '@redwoodjs/web';
+import { toast } from '@redwoodjs/web/dist/toast';
 
-import { QUERY } from 'src/components/Product/ProductsCell'
+import { QUERY } from '../EditProductCell';
 
 const DELETE_PRODUCT_MUTATION = gql`
   mutation DeleteProductMutation($id: Int!) {
@@ -20,6 +24,7 @@ const DELETE_PRODUCT_MUTATION = gql`
 `
 
 const ProductsList = ({ products }: FindProducts) => {
+
   const [deleteProduct] = useMutation(DELETE_PRODUCT_MUTATION, {
     onCompleted: () => {
       toast.success('Produto deletado!')
@@ -45,8 +50,118 @@ const ProductsList = ({ products }: FindProducts) => {
       as="main"
       maxW={'100%'}
       h={'100%'}
-      p={'10'}
-    ></Container>
+      px={'8'}
+      py={'12'}
+      style={{ backgroundColor: '#212529' }}
+    >
+      <Flex
+        as="section"
+        width={'100%'}
+        h={'auto'}
+        flexDirection={'column'}
+        alignItems={'center'}
+        justifyContent={'center'}
+        gap={'5rem'}
+      >
+        <Box
+          as="div"
+          w={'60%'}
+          display={'flex'}
+          flexDirection={'row'}
+          alignItems={'center'}
+          justifyContent={'space-around'}
+          gap={'3rem'}
+        >
+          <Input
+            placeholder="Pesquisar o nome do produto..."
+            bg={'whiteAlpha.800'}
+            color={'blackAlpha.900'}
+          />
+
+          <IconButton
+            aria-label="Search database"
+            icon={<SearchIcon />}
+            colorScheme="gray"
+          />
+        </Box>
+
+        <Center width={'100%'} h={'auto'}>
+          <Heading as="h2" color={'white'} cursor={'default'}>
+            {products.length} produtos disponíveis
+          </Heading>
+        </Center>
+
+        <Center width={'100%'} h={'auto'}>
+          <Grid
+            w={'100%'}
+            templateColumns="repeat(3, 1fr)"
+            templateRows={'auto'}
+            p={'6'}
+            gap={10}
+          >
+            {products.map((product) => (
+              <GridItem
+                key={product.id}
+                w={'100%'}
+                minH={'415px'}
+                p={'4'}
+                textAlign={'center'}
+                bg={'whiteAlpha.900'}
+                borderRadius={'10px'}
+                className="grid_item"
+              >
+                <Box
+                  w={'100%'}
+                  display={'flex'}
+                  flexDirection={'column'}
+                  justifyContent={'center'}
+                  alignItems={'center'}
+                  className="box_product"
+                >
+                  <Heading
+                    as="h3"
+                    fontSize={'1.4rem'}
+                    fontWeight={'bold'}
+                    textAlign={'center'}
+                  >
+                    {product.name}
+                  </Heading>
+
+                  <img
+                    src={product.image}
+                    alt={`Produto ${product.name}`}
+                    className="product_img"
+                  />
+
+                  <Stack gap={'1rem'}>
+                    <Text fontSize={'1.2rem'} textAlign={'center'}>
+                      Marca: <strong>{product.brand}</strong>
+                    </Text>
+
+                    <Text fontSize={'1.2rem'} textAlign={'center'}>
+                      Preço: R$ <strong>{product.price}</strong>
+                    </Text>
+                  </Stack>
+                </Box>
+              </GridItem>
+            ))}
+          </Grid>
+        </Center>
+
+        <Link to={routes.newProduct()}>
+          <IconButton
+            isRound={true}
+            variant="solid"
+            colorScheme="gray"
+            aria-label="Create"
+            fontSize="3rem"
+            w={'80px'}
+            h={'80px'}
+            icon={<SmallAddIcon />}
+          />
+        </Link>
+      </Flex>
+    </Container>
   )
 }
 
